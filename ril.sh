011df3a8369d48e2cbe119b48da4b0a3de7e6291
rilfv <51865391+rilfv@users.noreply.github.com>
2019-06-20 06:13:44 +0700

    new

---
+++ ril.sh
@@ -0,0 +1,23 @@
+clear
+echo "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷
+
+                 Script Memperbarui Tampilan
+                          By Mr.Ril
+
+÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷" | lolcat
+sleep 2
+echo "Ingin lanjut? [Y/n]" | lolcat
+read nomor
+
+if [ $nomor = Y ] || [ $nomor = y ]
+clear
+then
+pkg install ruby
+gem install lolcat
+pkg install nano
+pkg install bash
+pkg install figlet
+pkg install cowsay
+pkg install bash
+nano .bashrc
+fi
